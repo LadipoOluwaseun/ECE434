0a898920448c18608d42f00f25a1c640c917e8e8
Oluwaseun Ladipo <ladipooo@rose-hulman.edu>
2019-11-15 11:59:15 -0500

    added to servotest.js

--- Final Project/ServoTest.js
+++ Final Project/ServoTest.js
@@ -5,7 +5,7 @@ const b = require('bonescript');
 const util = require('util');
 
 const LED0 = 'USR3';
-const VSERV = 'P9_14'
+const VSERV = 'P9_14';
 const button = 'P9_25';
 
 b.pinMode(LED0, b.OUTPUT);
@@ -16,18 +16,25 @@ const AUTH = 'es3WljyRao1a-bphIXsF93x-ZBiph0Pr';
 
 var blynk = new Blynk.Blynk(AUTH);
 
+
 var v0 = new blynk.VirtualPin(0);
 var v1 = new blynk.VirtualPin(1);
+
+var v2 = new blynk.setProperty(V1, "url", "https://www.youtube.com/watch?v=EngW7tLk6R8");
+
 var v10 = new blynk.WidgetLED(10);
 
 v0.on('write', function(param) {
-    console.log('V0:', param[0]);
-    b.digitalWrite(LED0, param[0]);
+    //var duty_cycle = param[0]/1023;
+	var duty_cycle = param[0];
+	console.log('Vertical:', duty_cycle);
+	b.analogWrite(VSERV, duty_cycle);
 });
 
 v1.on('write', function(param) {
-	var duty_cycle = param[0]/1023;
-	console.log('Vertical:', duty_cycle);
+	//var duty_cycle = param[0]/1023;
+	var duty_cycle = param[0];
+	console.log('Horizontal:', duty_cycle);
 	b.analogWrite(VSERV, duty_cycle);
 });
 
